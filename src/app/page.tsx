a2fd89fbf38028f8da61aabad977554fd6165407
Quentin Vanderlinden <quentin.vanderlinden@gmail.com>
2024-12-03 14:54:41 +0100

    fix: suspense boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,29 @@
 
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+
 import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   const searchParams = useSearchParams();
   const nextPath = searchParams.get("next");
   return (
-    <div className="flex flex-col gap-4 items-center">
-      <div className="text-4xl italic text-red-600">
-        Welcome to Advent of Manon 2024 !
-      </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex flex-col gap-4 items-center">
+        <div className="text-4xl italic text-red-600">
+          Welcome to Advent of Manon 2024 !
+        </div>
 
-      {nextPath !== null && (
-        <Link
-          className={buttonVariants({ variant: "outline" })}
-          href={`/${nextPath}`}
-        >
-          See today&apos;s gift
-        </Link>
-      )}
-    </div>
+        {nextPath !== null && (
+          <Link
+            className={buttonVariants({ variant: "outline" })}
+            href={`/${nextPath}`}
+          >
+            See today&apos;s gift
+          </Link>
+        )}
+      </div>
+    </Suspense>
   );
 }

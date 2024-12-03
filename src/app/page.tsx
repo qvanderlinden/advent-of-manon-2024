70ff39bc6675f73bbf5cf14ae1022d69a621a01c
Quentin Vanderlinden <quentin.vanderlinden@gmail.com>
2024-12-03 14:57:37 +0100

    fix: suspense usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,31 @@ import { Suspense } from "react";
 
 import { buttonVariants } from "@/components/ui/button";
 
-export default function Home() {
+function Component() {
   const searchParams = useSearchParams();
   const nextPath = searchParams.get("next");
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="flex flex-col gap-4 items-center">
-        <div className="text-4xl italic text-red-600">
-          Welcome to Advent of Manon 2024 !
-        </div>
-
-        {nextPath !== null && (
-          <Link
-            className={buttonVariants({ variant: "outline" })}
-            href={`/${nextPath}`}
-          >
-            See today&apos;s gift
-          </Link>
-        )}
+    <div className="flex flex-col gap-4 items-center">
+      <div className="text-4xl italic text-red-600">
+        Welcome to Advent of Manon 2024 !
       </div>
+
+      {nextPath !== null && (
+        <Link
+          className={buttonVariants({ variant: "outline" })}
+          href={`/${nextPath}`}
+        >
+          See today&apos;s gift
+        </Link>
+      )}
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Component />
     </Suspense>
   );
 }

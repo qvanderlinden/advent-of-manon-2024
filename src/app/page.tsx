6f778a53f922559e6145efffe1a0c4ea86926c90
Quentin Vanderlinden <quentin.vanderlinden@gmail.com>
2024-12-03 14:50:13 +0100

    feat: add see today's gift link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { buttonVariants } from "@/components/ui/button";
+
 export default function Home() {
+  const searchParams = useSearchParams();
+  const nextPath = searchParams.get("next");
   return (
-    <div className="text-4xl italic text-red-600">
-      Welcome to Advent of Manon 2024 !
+    <div className="flex flex-col gap-4 items-center">
+      <div className="text-4xl italic text-red-600">
+        Welcome to Advent of Manon 2024 !
+      </div>
+
+      {nextPath !== null && (
+        <Link
+          className={buttonVariants({ variant: "outline" })}
+          href={`/${nextPath}`}
+        >
+          See today&apos;s gift
+        </Link>
+      )}
     </div>
   );
 }
